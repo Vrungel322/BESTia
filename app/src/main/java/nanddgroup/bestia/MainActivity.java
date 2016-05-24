e3d7e7fc12c4af0c761905b67dd72491015eca8a
Nikita Romanenko <nikita241296@gmail.com>
2016-05-24 21:05:22 +0300

    image showing

--- app/src/main/java/nanddgroup/bestia/MainActivity.java
+++ app/src/main/java/nanddgroup/bestia/MainActivity.java
@@ -1,16 +1,20 @@
 package nanddgroup.bestia;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.util.Log;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
-import butterknife.BindView;
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import nanddgroup.bestia.Utils.JsonHelper;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.ivPst0)
+    @Bind(R.id.ivPst0)
     ImageView ivPst0;
     String[] pst;
 
@@ -23,6 +27,13 @@ public class MainActivity extends AppCompatActivity {
         pst = JsonHelper.getB64DataFromJson(JsonHelper.loadJSONFromAsset(getApplicationContext()));
 
         Log.wtf("wtf", pst[0]);
+
+        byte[] decodedString = Base64.decode(pst[0].getBytes(), Base64.DEFAULT);
+        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(decodedByte.getWidth(), decodedByte.getHeight());
+        layoutParams.setMargins(236, 0 , 0, 1790);
+        ivPst0.setLayoutParams(layoutParams);
+        ivPst0.setImageBitmap(decodedByte);
     }
 
 }
