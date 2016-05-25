2769d65c17dd3ec700666dd6d85e8124b355e713
Nikita Romanenko <nikita241296@gmail.com>
2016-05-25 16:09:25 +0300

    Poster 2 ready

--- app/src/main/java/nanddgroup/bestia/MainActivity.java
+++ app/src/main/java/nanddgroup/bestia/MainActivity.java
@@ -18,8 +18,12 @@ public class MainActivity extends AppCompatActivity {
     public static final int H_POSTER = 318;
     public static final int X_POSTER1 = 236;
     public static final int Y_POSTER1 = 1790;
+    public static final int X_POSTER2 = 440;
+    public static final int Y_POSTER2 = 1850;
     @Bind(R.id.ivPst0)
     ImageView ivPst0;
+    @Bind(R.id.ivPst1)
+    ImageView ivPst1;
     @Bind(R.id.imageView)
     ImageView imageView;
     String[] pst;
@@ -34,14 +38,18 @@ public class MainActivity extends AppCompatActivity {
 
         Log.wtf("wtf", pst[0]);
 
-        byte[] decodedString = Base64.decode(pst[0].getBytes(), Base64.DEFAULT);
+        poster_1(pst[0], X_POSTER1, Y_POSTER1, ivPst0, 3f);
+        poster_1(pst[1], X_POSTER2, Y_POSTER2, ivPst1, -1f);
+    }
+
+    private void poster_1(String pst_cur, int x_location, int y_location, ImageView iv, float angle) {
+        byte[] decodedString = Base64.decode(pst_cur.getBytes(), Base64.DEFAULT);
         Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
         RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(decodedByte.getWidth(), decodedByte.getHeight());
-        ivPst0.setRotation(8.021409f);
-        ivPst0.setImageBitmap(Bitmap.createScaledBitmap(decodedByte, W_POSTER, H_POSTER, false));
-        Log.wtf("wtf", String.valueOf(ivPst0.getWidth()/2));
-        layoutParams.setMargins(X_POSTER1, Y_POSTER1, 0, 0);
-        ivPst0.setLayoutParams(layoutParams);
+        iv.setRotation(angle);
+        iv.setImageBitmap(Bitmap.createScaledBitmap(decodedByte, W_POSTER, H_POSTER, false));
+        layoutParams.setMargins(x_location, y_location, 0, 0);
+        iv.setLayoutParams(layoutParams);
     }
 
 }
