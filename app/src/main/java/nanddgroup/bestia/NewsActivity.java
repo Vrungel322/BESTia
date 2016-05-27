41873335a310c9296ae3134583fb9faa220f1a90
Nikita Romanenko <nikita241296@gmail.com>
2016-05-28 01:09:08 +0300

    list - not done

--- app/src/main/java/nanddgroup/bestia/NewsActivity.java
+++ app/src/main/java/nanddgroup/bestia/NewsActivity.java
@@ -2,16 +2,25 @@ package nanddgroup.bestia;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
+
+import org.json.JSONException;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import nanddgroup.bestia.Utils.JsonHelper;
+import nanddgroup.bestia.Utils.NewsAdapter;
 
 public class NewsActivity extends AppCompatActivity {
     @Bind(R.id.news_best)
@@ -20,6 +29,14 @@ public class NewsActivity extends AppCompatActivity {
     ImageView news;
     @Bind(R.id.bBackToMain)
     Button bBackToMain;
+//    @Bind(R.id.lvNews)
+//    ListView lvNews;
+    private NewsAdapter nA;
+    private ArrayList<String> pst;
+    private String json;
+    private ArrayList<Bitmap> alBitmaps;
+    private ListView lvNews;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +44,33 @@ public class NewsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_news);
         ButterKnife.bind(this);
         this.overridePendingTransition(R.anim.slide_left_in, R.anim.slide_left_out);
-
-        Drawable myDrawable = getResources().getDrawable(R.drawable.news);
-        Bitmap news_background = ((BitmapDrawable) myDrawable).getBitmap();
-        news.setImageBitmap(Bitmap.createScaledBitmap(news_background,
-                getWindowManager().getDefaultDisplay().getWidth(),
-                getWindowManager().getDefaultDisplay().getHeight(), true));
+        pst = new ArrayList<String>();
+        alBitmaps = new ArrayList<Bitmap>();
 
 
-        Drawable myDrawable1 = getResources().getDrawable(R.drawable.news_best);
-        Bitmap news_background1 = ((BitmapDrawable) myDrawable1).getBitmap();
-        news_best.setImageBitmap(news_background1);
+        initMainBackground();
+        json = JsonHelper.loadJSONFromAsset(getApplicationContext(), "uk-news.json");
+        pst = JsonHelper.getB64DataFromJson(json);
+        try {
+            for (int i = 0; i < JsonHelper.getCount(json); i++){
+                alBitmaps.add(poster_2(pst.get(i)));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        nA = new NewsAdapter(getApplicationContext(), R.layout.each_news_for_lv, alBitmaps);
+        lvNews = (ListView) findViewById(R.id.lvNews);
+        lvNews.setAdapter(nA);
+
+
+
+    }
+
+    private Bitmap poster_2(String pst_cur) {
+        byte[] decodedString = Base64.decode(pst_cur.getBytes(), Base64.DEFAULT);
+        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        return decodedByte;
+
     }
 
     @OnClick(R.id.bBackToMain)
@@ -52,4 +85,17 @@ public class NewsActivity extends AppCompatActivity {
         startActivity(new Intent(NewsActivity.this, MainActivity.class));
         finish();
     }
+
+    private void initMainBackground() {
+        Drawable myDrawable = getResources().getDrawable(R.drawable.news);
+        Bitmap news_background = ((BitmapDrawable) myDrawable).getBitmap();
+        news.setImageBitmap(Bitmap.createScaledBitmap(news_background,
+                getWindowManager().getDefaultDisplay().getWidth(),
+                getWindowManager().getDefaultDisplay().getHeight(), true));
+
+        Drawable myDrawable1 = getResources().getDrawable(R.drawable.news_best);
+        Bitmap news_background1 = ((BitmapDrawable) myDrawable1).getBitmap();
+        news_best.setImageBitmap(news_background1);
+    }
+
 }
---
+++ app/src/main/java/nanddgroup/bestia/Utils/NewsAdapter.java
@@ -0,0 +1,54 @@
+package nanddgroup.bestia.Utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+import nanddgroup.bestia.R;
+
+/**
+ * Created by Nikita on 27.05.2016.
+ */
+public class NewsAdapter extends ArrayAdapter<Bitmap> {
+    Context context;
+    ArrayList<Bitmap> alNewsPosters;
+
+
+    public NewsAdapter(Context context, int resource, ArrayList<Bitmap> alNewsPosters) {
+        super(context, resource, alNewsPosters);
+        this.context = context;
+        this.alNewsPosters = alNewsPosters;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View rootView = null;
+        if (convertView == null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            rootView = inflater.inflate(R.layout.each_news_for_lv, parent, false);
+        }
+        else {
+            rootView = convertView;
+        }
+        ImageView ivHeader = (ImageView) rootView.findViewById(R.id.ivHeader);
+//        Toast.makeText(context, "getView" + position, Toast.LENGTH_SHORT).show();
+        if (position % 2 == 0) {
+            ivHeader.setImageResource(R.drawable.news_poster_sprt_1);
+        }
+        else {
+            ivHeader.setImageResource(R.drawable.news_poster_sprt_0);
+        }
+        ImageView ivFrame = (ImageView) rootView.findViewById(R.id.ivFrame);
+        ivFrame.setImageResource(R.drawable.news_poster_frame);
+        ImageView ivPoster = (ImageView) rootView.findViewById(R.id.ivPoster);
+        ivPoster.setImageBitmap(alNewsPosters.get(position));
+
+        return rootView;
+    }
+}
