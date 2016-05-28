8895efa9a3bb3d878df8ad4776f6872852680038
Nikita Romanenko <nikita241296@gmail.com>
2016-05-28 11:32:44 +0300

    logo, bitmap loader, scaler added

--- app/src/main/java/nanddgroup/bestia/Utils/NewsAdapter.java
+++ app/src/main/java/nanddgroup/bestia/Utils/NewsAdapter.java
@@ -1,7 +1,9 @@
 package nanddgroup.bestia.Utils;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -37,23 +39,35 @@ public class NewsAdapter extends ArrayAdapter<Bitmap> {
             rootView = convertView;
         }
         ImageView ivFrame = (ImageView) rootView.findViewById(R.id.ivFrame);
-        ivFrame.setImageResource(R.drawable.news_poster_frame);
         ImageView ivPoster = (ImageView) rootView.findViewById(R.id.ivPoster);
-        ivPoster.setImageBitmap(alNewsPosters.get(position));
+        ivPoster.setImageBitmap(scaleBitmap(alNewsPosters.get(position), 0.35d));
         ImageView ivHeader = (ImageView) rootView.findViewById(R.id.ivHeader);
-//        Toast.makeText(context, "getView" + position, Toast.LENGTH_SHORT).show();
         if (position % 2 == 0) {
-            ivHeader.setImageResource(R.drawable.news_poster_sprt_0);
+            ivHeader.setImageBitmap(bitmapLoader(context.getResources(), R.drawable.news_poster_sprt_0, 0.5d));
+            ivFrame.setImageBitmap(bitmapLoader(context.getResources(), R.drawable.news_poster_frame, 0.35d));
             ivFrame.setRotation(1f);
             ivPoster.setRotation(1f);
         }
         else {
-            ivHeader.setImageResource(R.drawable.news_poster_sprt_1);
+            ivHeader.setImageBitmap(bitmapLoader(context.getResources(), R.drawable.news_poster_sprt_1, 0.5d));
+            ivFrame.setImageBitmap(bitmapLoader(context.getResources(), R.drawable.news_poster_frame, 0.35d));
             ivFrame.setRotation(-0.7f);
             ivPoster.setRotation(-0.7f);
         }
-
-
         return rootView;
+    }
+
+    private Bitmap bitmapLoader(Resources res, int resId, double convert) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        options.inPreferredConfig = Bitmap.Config.RGB_565;
+        options.inDither = true;
+        options.inJustDecodeBounds = false;
+        Bitmap bitmap = BitmapFactory.decodeResource(res, resId, options);
+        return Bitmap.createScaledBitmap(bitmap, (int) (((double) bitmap.getWidth()) * convert), (int) (((double) bitmap.getHeight()) * convert), false);
+    }
+
+    private Bitmap scaleBitmap(Bitmap bitmap, double convert){
+        return Bitmap.createScaledBitmap(bitmap, (int) (((double) bitmap.getWidth()) * convert), (int) (((double) bitmap.getHeight()) * convert), false);
     }
 }
